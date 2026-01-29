b175d95a982c2efde9a008683a33a85918a4944d
lovelyshopping <lovelyshopping39@gmail.com>
2026-01-29 14:03:37 +0900

    Add shipping partners data file

---
+++ data/shipping-partners.js
@@ -0,0 +1,6 @@
+// data/shipping-partners.js
+export const shippingPartners = [
+  { id: "onegai", name: "Onegai Cargo", note: "Japan → Indonesia specialist" },
+  { id: "portasi", name: "Portasi", note: "Door-to-port options" },
+  { id: "manual", name: "Other (confirm via WA)", note: "We will confirm cost" }
+];
