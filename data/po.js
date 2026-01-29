2b9c2e0b18d8163b26401afd76ba3705a28ff2ba
lovelyshopping <lovelyshopping39@gmail.com>
2026-01-29 14:03:18 +0900

    Create po.js with PO status and close date
    Added PO status and close date for site behavior control.

---
+++ data/po.js
@@ -0,0 +1,6 @@
+// data/po.js
+// PO status and close date: change to control site behavior
+export const po = {
+  status: "OPEN", // "OPEN" or "CLOSED"
+  closeDate: "2026-02-28" // ISO date for display & batch id
+};
