b3b021a33782f123ccdf498b682198b19d2275dc
lovelyshopping <lovelyshopping39@gmail.com>
2026-01-29 14:02:17 +0900

    Create categories.js

---
+++ data/categories.js
@@ -0,0 +1,10 @@
+// data/categories.js
+// Edit or extend categories here. id should match product.category.
+export const categories = [
+  { id: "all", name: "All" },
+  { id: "pokemon", name: "Pokémon" },
+  { id: "onepiece", name: "One Piece" },
+  { id: "figures", name: "Figures" },
+  { id: "plush", name: "Plushies" },
+  { id: "accessories", name: "Accessories" }
+];
