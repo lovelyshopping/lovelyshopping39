40f812e7fb72a27469cb28be58f3da749e9a267e
lovelyshopping <lovelyshopping39@gmail.com>
2026-01-29 14:04:12 +0900

    Create forms.js

---
+++ data/forms.js
@@ -0,0 +1,12 @@
+// data/forms.js
+// Put your Google Form prefilled URLs & WhatsApp merchant number here.
+// After you create the forms, paste the prefilled templates below.
+export const FORMS = {
+  order: {
+    // Example template (replace entry.xxx and YOUR_FORM_ID with values from your form):
+    // urlTemplate: 'https://docs.google.com/forms/d/e/YOUR_ORDER_FORM_ID/viewform?usp=pp_url&entry.111111={product}&entry.222222={buyer_name}&entry.333333={buyer_wa}&entry.444444={buyer_addr}&entry.555555={partner}&entry.666666={total}&entry.777777={po_batch}'
+    urlTemplate: ''
+  },
+  // WhatsApp merchant number (format: 62812... no +)
+  whatsappNumber: ''
+};
